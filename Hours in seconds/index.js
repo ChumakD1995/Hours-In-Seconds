61ff0a0ddbeb4061aec2ab329115ad18c06d570b
chumak8088@gmail.com <k.smulska@pro-capital.local>
2023-04-11 23:43:22 +0300

    hours in seconds

--- Hours in seconds/index.js
+++ Hours in seconds/index.js
@@ -1,20 +1,4 @@
-const num1 = parseFloat (prompt (""));
-const num2 = parseFloat (prompt (""));
-const sum = num1 + num2;
-console.log(sum)
-alert(sum) 
-const num3 = parseFloat (prompt (""));
-const num4 = parseFloat (prompt (""));
-const dev = num3 - num4;
-console.log(dev)
-alert(dev) 
-const num5 = parseFloat (prompt (""));
-const num6 = parseFloat (prompt (""));
-const mult = num5 * num6;
-console.log(mult)
-alert(mult) 
-const num7 = parseFloat (prompt (""));
-const num8 = parseFloat (prompt (""));
-const div = num7 / num8;
-console.log(div)
-alert(div) 
+let hours = prompt ("");
+const seconds = hours * 3600; 
+console.log(seconds);
+alert(`in ${hours} ${seconds}`)
